8b05a9472fdb8e089367f21a4ba4c767225907d0
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-10-27 12:22:00 +0900

    set ItemCounter in Basket

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import ItemCounter from "./ItemCounter";
 import "./Basket.css"
 
 const Basket = (props) => {
@@ -68,6 +67,33 @@ const Basket = (props) => {
           </ul>
         )}
       </div>
+    </div>
+  );
+};
+
+const ItemCounter = ({ counter, setCounter }) => {
+  const countUp = () => {
+    setCounter(counter + 1)
+  };
+  const countDown = () => {
+    counter && setCounter(counter - 1)
+  };
+  const resetCount = () => {
+    setCounter(0)
+  };
+
+  return (
+    <div className="wrapper">
+      <button onClick={countUp} className="add">
+        <div className="fa-solid fa-square-plus"></div>
+      </button>
+      <div className="quantity-count">{counter}</div>
+      <button onClick={countDown} className="remove">
+        <div className="fa-solid fa-square-minus"></div>
+      </button>
+      <button onClick={resetCount} className="reset">
+        <div className="fa-solid fa-trash-can"></div>
+      </button>
     </div>
   );
 };
