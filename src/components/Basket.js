171c27ed88f1ac05bf61b7b0554771a9e9b8c6fe
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-10-29 12:08:41 +0900

    add error log in Basket.js

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -28,22 +28,26 @@ const Basket = (props) => {
 const OrderResult = ({ toggle, cartItems }) => {
   const [order, setOrder] = useState({ name: "", count: 0 });
 
-  // 問題点 1
-  // それぞれのstateでカウントを取れるようになる。
+  // 問題点2、3については手を付けられていない。
+  // 問題点 2
+  // それぞれのstateでカウントを取れるようになったうえで、
+  // 品名とその種類または色に紐づいた数量をもって購入商品の合計を表示させる。
+  // いずれかの状態の合計を算出させる。
+  //   商品名 × 単価 × 数量 ＝ 料金
+  //   種類　 × 単価 × 数量 ＝ 料金
+  //   色　　 × 単価 × 数量 ＝ 料金
+
+  // 問題点 3
+  // 『送料』を表示させる。
+  //   送料
+  //     種類 重量 × 数量
+  //     色　 重量 × 数量
   // formで全国の都道府県のリスト作成。
-  // 合計金額や『送料』を表示させる。
-  // 要件
-  //   商品名
-  //     料金
-  //       種類 × 数量
-  //       色 × 数量
-  //     送料
-  //       種類 重量 × 数量
-  //       色 重量 × 数量
   // 都道府県ごとに係数をかけるとか？
   // const shippingTable = {
   //   100: {"京都": 1, "大阪": 1, "兵庫": 1, "奈良": 1, ...}, 
   // };
+
   // const itemsPrice = cartItems.reduce((sum, item) => sum + order.count * item.price, 0);
   // const taxPrice = itemsPrice * 0.1;
   // const totalPrice = itemsPrice + taxPrice;
@@ -94,13 +98,13 @@ const OrderResult = ({ toggle, cartItems }) => {
                 </li>))
               : <li className="display-none"></li>
           }
-          {/* 問題点 1 の関係箇所 */}
+          {/* 問題点 2 の関係箇所 */}
           {/* <li className="sub-total">
             {Math.round(cartItem.price)}円&nbsp;×&nbsp;{count}
           </li> */}
         </ul>
       ))}
-      {/* 問題点 1 の関係箇所 */}
+      {/* 問題点 2 の関係箇所 */}
       {/* {cartItems.length !== 0 && (
         <ul className="calc-amount">
           <li>商品小計<span>{Math.round(itemsPrice)}</span>円</li>
@@ -116,8 +120,24 @@ const OrderResult = ({ toggle, cartItems }) => {
 }
 
 const ItemCounter = ({ setName, count, order, setOrder }) => {
-  
+
   // setOrder(order => ({order, name: setName}));
+
+  // react-dom.development.js:86 Warning: 
+  // Cannot update a component (`OrderResult`) while rendering a different component (`ItemCounter`). 
+  // To locate the bad setState() call inside `ItemCounter`, follow the stack trace as described in 
+  // https://reactjs.org/link/setstate-in-render
+  //   at ItemCounter (http://localhost:3000/main.66ae1621d7f77cc7ca5d.hot-update.js:220:3)
+  //   at li
+  //   at ul
+  //   at div
+  //   at OrderResult (http://localhost:3000/main.66ae1621d7f77cc7ca5d.hot-update.js:82:3)
+  //   at div
+  //   at Basket (http://localhost:3000/main.66ae1621d7f77cc7ca5d.hot-update.js:30:5)
+  //   at div
+  //   at App (http://localhost:3000/static/js/bundle.js:39:84)
+
+  setOrder(order => ({order, name: setName}));
 
   const countUp = () => {
     setOrder(order => ({ ...order, count: count + 1 }));
