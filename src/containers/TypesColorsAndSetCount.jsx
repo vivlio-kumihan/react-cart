fdb65405bea180df427ca6a811367eb4a2e5c0aa
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-11-19 23:19:23 +0900

    refactoring TypesColorsAndSetCount

--- src/containers/TypesColorsAndSetCount.jsx
+++ src/containers/TypesColorsAndSetCount.jsx
@@ -11,67 +11,36 @@ const TypesColorsAndSetCount = ({
 
   return (
     <>
-      {
-        Object.keys(types).length !== 0 ? (
-        <ul className="types">
-          {Object.keys(types).map((key, idx) => (
+    {
+      hasItem(types) || hasItem(colors)
+        ? (
+          <ul className={switchItem[1]}>
+          {Object.keys(switchItem[0]).map((key, idx) => (
             <li key={idx}>
               <span>{key}</span>
               <input 
-                id="types"
+                id={switchItem[1]}
                 type="number" 
                 min="0"
                 onChange={(e)=>{
-                  types[key] = e.target.value
-                  setEachCount({...types, [key]: e.target.value})
+                  switchItem[0][key] = e.target.value
+                  setEachCount({...switchItem[0], [key]: e.target.value})
                 }}
                 placeholder="0"
                 // value属性を無しにするとそれぞれカウントできる。
-                // value={typesCount}
+                // value={typeCount}
               />
             </li>
           ))}
           {
             useEffect(() => {
-              whichItemSumCalcCount(types);
-            }, [types, whichItemSumCalcCount])
+              whichItemSumCalcCount(switchItem[0]);
+            }, [switchItem[0], whichItemSumCalcCount])
           }          
-        </ul>
-      ) : (
-        <ul className="display-none"></ul>
-      )}
-
-      {/* {Object.keys(colors).length !== 0 ? (
-        <ul className="colors">
-          {
-            Object.keys(colors).map((key, idx) => (
-            <li key={idx}>
-              <span>{key}</span>
-              <input 
-                id="colors"
-                type="number"
-                min="0" 
-                onChange={(e)=>{
-                  colors[key] = e.target.value
-                  setEachCount({...colors, [key]: e.target.value})                  
-                }}
-                placeholder="0"
-                // きっかけは、『たまたま』
-                // value属性を無しにするとそれぞれカウント
-                // できることを見つけたことだった。ラッキーだった。
-                // value={typesCount}
-              />
-            </li>
-          ))}
-          {
-            useEffect(() => {
-              whichItemSumCalcCount(colors);
-            }, [colors, whichItemSumCalcCount])
-          }
-        </ul>
-      ) : (
-        <ul className="display-none"></ul>
-      )} */}
+          </ul>
+        )
+        : (<ul className="display-none"></ul>)
+    }
     </>
   );
 };
